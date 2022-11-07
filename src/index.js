9e0f4ae8090bcefcb2f0a4650a00e28f5e3deae5
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-11-07 01:40:03 -0600

    Update index and re-adding App component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div >
-      Main component should be here
-    </div>
+    <App />
   </React.StrictMode>
 );
