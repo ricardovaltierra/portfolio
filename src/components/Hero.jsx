93acfbc98cc9a4e773f5306be857a927015d37d3
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-11-12 08:19:57 -0600

    Update classnames for paragraph and divs on section to finish

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,8 +3,8 @@ import styles from "../style";
 const Hero = () => {
   return (
     <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
-      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
-        <div className="flex flex-col  justify-between items-start w-full">
+      <div className={`flex-1 ${styles.flexStart} md:flex-row flex-col xl:px-0 sm:px-16 px-6`}>
+        <div className="flex flex-col justify-between items-start w-full">
           <h1 className="flex-1 font-worksans font-light ss:text-[92px] text-[62px] text-dimWhite ss:leading-[130px] leading-[70px]">
             Ricardo {" "}
             <span>Valtierra</span> {" "}
@@ -20,7 +20,7 @@ const Hero = () => {
             </span>
           </h2>
 
-          <p className={`${styles.paragraph} max-w-[550px]`}>
+          <p className={`${styles.paragraph} max-w-[550px] my-5`}>
             Enjoying software engineering, systems design, (back&&front) development, and accessibility.
           </p>
         </div>
