9c815b23ae5e860fd02393acc86e242a39ce650e
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-11-11 20:02:21 -0600

    Begin responsive Navbar with header and title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,18 @@
-import { React } from 'react';
-import { close, profile, menu } from '../assets';
+import React from 'react';
 import { navLinks } from '../constants';
 
-const Navbar = () => {
-  return (
-    <nav className="w-full flex py-6 justify-between items-center navbar">
-      <img src='https://firebasestorage.googleapis.com/v0/b/personal-portfolio-e6b7b.appspot.com/o/profile.png?alt=media&token=2cd2797e-0be7-4cfc-9f3f-ed2ea1cb584c' alt="personal photography" className='w-[70px] h-[70px]' />
-      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
-        {navLinks.map((nav, index) => (
-          <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? "mr-0" : "mr-10"} text-white`} >
-            <a href={`#${nav.id}`}>
-              {nav.title}
-            </a>
-          </li>
-        ))}
-      </ul>
+const Navbar = () => (
+  <header className="">
+    <h3>Logo</h3>
+    <nav>
+      {navLinks.map((nav, index) => (
+          <a key={index} href={`#${nav.id}`}>
+            <div className="">{nav.title}</div>
+          </a>
+        ))
+      }
     </nav>
-  )
-};
+  </header>
+  );
 
 export default Navbar;
