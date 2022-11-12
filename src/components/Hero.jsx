d62623fba7fb2a5fb18bf74b91cbd653ad0bfe7e
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-11-12 08:31:07 -0600

    Adjust margin and leading for headers

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,12 +5,12 @@ const Hero = () => {
     <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
       <div className={`flex-1 ${styles.flexStart} md:flex-row flex-col xl:px-0 sm:px-16 px-6`}>
         <div className="flex flex-col justify-between items-start w-full">
-          <h1 className="flex-1 font-worksans font-light ss:text-[92px] text-[62px] text-dimWhite ss:leading-[130px] leading-[70px]">
+          <h1 className="flex-1 font-worksans font-light ss:text-[92px] text-[62px] text-dimWhite ss:leading-[120px] leading-[70px]">
             Ricardo {" "}
             <span>Valtierra</span> {" "}
           </h1>
           
-          <h2 className="text-dimRose md:mt-0 mt-3 font-worksans font-light ss:text-[45px] text-[40px] ss:leading-[100px] leading-[45px]">
+          <h2 className="text-dimRose md:mt-0 mt-4 font-worksans font-light ss:text-[45px] text-[40px] ss:leading-[100px] leading-[45px]">
             Developer | <br className="sm:hidden block" /> {" "}
             <span className="ss:text-[40px] text-[35px]">
               RoR {" "}
