7bb31b9e8b973767951e048172bd4ec1fe37e780
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-11-11 23:41:33 -0600

    Set profile photo on header and send to right nav element

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,8 +11,14 @@ const Navbar = () => {
 
   return (
   <header className="bg-blue-900 flex items-center justify-between navbar h-[80px] py-0 px-8 text-white">
-    <h3>Logo</h3>
-    <nav ref={navRef} className="bg-blue-900">
+    <h3>
+      <img
+        src='https://firebasestorage.googleapis.com/v0/b/personal-portfolio-e6b7b.appspot.com/o/profile.png?alt=media&token=2cd2797e-0be7-4cfc-9f3f-ed2ea1cb584c'
+        alt="personal photography"
+        className='w-[70px] h-[70px]'
+      />
+    </h3>
+    <nav ref={navRef} className="bg-blue-900 ml-auto">
       {navLinks.map((nav, index) => (
           <a key={index} href={`#${nav.id}`} className="my-0 mx-8 no-underline" onClick={showNavbar}>
             {nav.title}
