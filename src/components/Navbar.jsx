6c10f6c0f7e488f9cb4756997bd695609a77a498
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-11-11 20:21:45 -0600

    Add react icons and set them with useRef and class toggle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,33 @@
-import React from 'react';
+import { useRef } from 'react';
+import { FaBars, FaTimes } from "react-icons/fa";
 import { navLinks } from '../constants';
 
-const Navbar = () => (
+const Navbar = () => {
+  const navRef = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("reponsive_nav");
+  }
+
+  return (
   <header className="">
     <h3>Logo</h3>
-    <nav>
+    <nav ref={navRef}>
       {navLinks.map((nav, index) => (
           <a key={index} href={`#${nav.id}`}>
             <div className="">{nav.title}</div>
           </a>
         ))
       }
+      <button onClick={showNavbar} className="nav-btn nav-close-btn">
+        <FaTimes />
+      </button>
     </nav>
+    <button onClick={showNavbar} className="nav-btn">
+      <FaBars />
+    </button>
   </header>
-  );
+  )
+};
 
 export default Navbar;
