636e7da2a64efe34d471e5c0d15de62bf9bcd06f
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-11-11 23:20:26 -0600

    Update tag classes, media query rules and fix toggle class typo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,15 +6,15 @@ const Navbar = () => {
   const navRef = useRef();
 
   const showNavbar = () => {
-    navRef.current.classList.toggle("reponsive_nav");
+    navRef.current.classList.toggle("responsive_nav");
   }
 
   return (
-  <header className=" bg-blue-900 w-full flex items-center justify-between navbar h-[80px] py-0 px-8 text-white">
+  <header className="bg-blue-900 flex items-center justify-between navbar h-[80px] py-0 px-8 text-white">
     <h3>Logo</h3>
-    <nav ref={navRef}>
+    <nav ref={navRef} className="bg-blue-900">
       {navLinks.map((nav, index) => (
-          <a key={index} href={`#${nav.id}`} className="my-0 mx-8 no-underline">
+          <a key={index} href={`#${nav.id}`} className="my-0 mx-8 no-underline" onClick={showNavbar}>
             {nav.title}
           </a>
         ))
