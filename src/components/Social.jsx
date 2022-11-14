599c2b68e31adbd7723cce55f7cd8f6edda65ba8
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-11-13 21:33:37 -0600

    Add icon for resume, grid for container and margin inside

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,19 +1,19 @@
 import { layout } from '../style';
 import { FaLinkedin, FaGithub, FaTwitter, FaWhatsapp } from 'react-icons/fa';
 import { MdMail } from 'react-icons/md';
+import { AiFillFile } from 'react-icons/ai';
 
 const Social = () => {
   return (
-    <div>
-      <div className={`${layout.hero} text-dimRose h-full gap-x-6 md:mt-0 mt-2`}>
-          <div className="si-container"><FaLinkedin /></div>
-          <div className="md:flex hidden si-container"><FaGithub /></div>
-          <div className="md:flex hidden si-container"><FaTwitter /></div>
-          <div className="si-container"><MdMail /></div>
-          <div className="si-container"><FaWhatsapp /></div>
-        </div>
+    <div className={`w-full ${layout.hero} text-dimRose grid grid-cols-3 gap-8 md:mt-0 mt-2`}>
+      <div className="si-container"><FaLinkedin /></div>
+      <div className="md:flex hidden si-container"><FaGithub /></div>
+      <div className="md:flex hidden si-container"><FaTwitter /></div>
+      <div className="si-container"><MdMail /></div>
+      <div className="si-container"><FaWhatsapp /></div>
+      <div className="si-container"><AiFillFile /></div>
     </div>
   )
 }
 
-export default Social
+export default Social;
