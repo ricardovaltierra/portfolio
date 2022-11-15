8f9acc5924711a6f7be45dba007e796963a47569
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-11-15 13:47:10 -0600

    Set container for cards and update style

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import Card from './Card';
 
 const Portfolio = () => (
-  <div className="w-full flex flex-col justify-center md:my-[80px] my-[50px]">
-    <h2 className="text-dimWhite md:my-0 my-9 font-worksans font-thin ss:text-[45px] text-[40px] ss:leading-[100px] leading-[45px]">
+  <div className="w-full flex flex-col justify-center md:my-[90px] my-[50px]">
+    <h2 className="text-dimWhite md:my-0 my-12 font-worksans font-light ss:text-[45px] text-[40px] ss:leading-[100px] leading-[45px] ">
       Featured
       <br className="sm:hidden block" />
       {' '}
       Projects
     </h2>
-    <Card />
+    <div className="flex md:flex-row flex-col items-center md:my-5 my-8">
+      <Card />
+    </div>
   </div>
 );
 
