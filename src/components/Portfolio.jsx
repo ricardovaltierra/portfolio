e5a79377d92a061fc592813e2c93acd2ccc6c1c9
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-11-15 14:09:15 -0600

    Map projects array with Card and disable props eslint rule

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { projectsContent } from '../constants';
 import Card from './Card';
 
 const Portfolio = () => (
@@ -6,11 +7,12 @@ const Portfolio = () => (
     <h2 className="text-dimWhite md:my-0 my-12 font-worksans font-light ss:text-[45px] text-[40px] ss:leading-[100px] leading-[45px] ">
       Featured
       <br className="sm:hidden block" />
-      {' '}
       Projects
     </h2>
     <div className="flex md:flex-row flex-col items-center md:my-5 my-8">
-      <Card />
+      {projectsContent.map((item) => (
+        <Card key={item.id} {...item} />
+      ))}
     </div>
   </div>
 );
