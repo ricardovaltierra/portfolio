cb5154eef3fb84588e94c724d7a80eecfaed2799
Ricardo Valtierra <ricardo_valtierra@outlook.com>
2022-11-17 09:56:04 -0600

    Revert "Feature/add image url and metatags"

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,24 @@ import styles from './style';
 import {
   Navbar, Hero, Social, Portfolio,
 } from './components';
-import SEO from './components/SEO';
 
 const App = () => (
-  <>
-    <SEO />
-    <div className="bg-primary">
-      <Navbar />
+  <div className="bg-primary">
+    <Navbar />
 
-      <div className={`bg-primary ${styles.flexStart} mt-10`}>
-        <div className={`${styles.boxWidth}`}>
-          <Hero />
-        </div>
-      </div>
-
-      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>
-          <Social />
-          <Portfolio />
-        </div>
+    <div className={`bg-primary ${styles.flexStart} mt-10`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
       </div>
     </div>
-  </>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Social />
+        <Portfolio />
+      </div>
+    </div>
+  </div>
 );
 
 export default App;
