52737f0c026f328a971912944fc1f7b9eb772742
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-11-19 21:41:50 -0600

    Create About component and fill Navabar with Link and NavLink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useRef } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 import { IoMdClose } from 'react-icons/io';
 import { HiOutlineBars3 } from 'react-icons/hi2';
 import { navLinks } from '../constants';
@@ -12,26 +13,28 @@ const Navbar = () => {
 
   return (
     <header className="bg-secondary flex items-center justify-between navbar h-[80px] py-0 px-8 text-white">
-      <h3 className="border-[2px] border-dimWhite p-[3px] rounded-full md:ml-9">
-        <img
-          src="https://firebasestorage.googleapis.com/v0/b/personal-portfolio-e6b7b.appspot.com/o/profile.png?alt=media&token=2cd2797e-0be7-4cfc-9f3f-ed2ea1cb584c"
-          alt="personal photography"
-          className="w-[40px] h-[40px]"
-        />
-      </h3>
+      <Link to="/" className="nav-image" onclick={showNavbar}>
+        <h3 className="border-[2px] border-dimWhite p-[3px] rounded-full md:ml-9">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/personal-portfolio-e6b7b.appspot.com/o/profile.png?alt=media&token=2cd2797e-0be7-4cfc-9f3f-ed2ea1cb584c"
+            alt="personal photography"
+            className="w-[40px] h-[40px]"
+          />
+        </h3>
+      </Link>
       <nav
         ref={navRef}
         className="bg-secondary md:flex md:place-items-center ml-auto"
       >
         {navLinks.map((nav) => (
-          <a
-            key={nav.id}
-            href={`#${nav.id}`}
-            className="my-0 mx-8 no-underline"
+          <NavLink
+            to="/"
             onClick={showNavbar}
+            className="my-0 mx-8 no-underline nav-links"
+            key={nav.id}
           >
             {nav.title}
-          </a>
+          </NavLink>
         ))}
         <button
           onClick={showNavbar}
