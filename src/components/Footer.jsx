f20dfc04318a91eb9fe05b5f388020268c8b0a0a
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-11-23 11:48:22 -0600

    Reorder Footer URLs and add Wellfound one

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,10 @@
 import { AiFillFile } from 'react-icons/ai';
-import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
-import { MdMail } from 'react-icons/md';
+import {
+  FaAngellist,
+  FaGithub,
+  FaLinkedin,
+  FaTwitter,
+} from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import styles, { layout } from '../style';
 
@@ -45,13 +49,13 @@ const Footer = () => (
             <span className={styles.linkHover}>GitHub</span>
           </a>
           <a
-            href="mailto:ricardo_valtierra@outlook.com"
+            href="https://angel.co/ricardo-valtierra"
             target="_blank"
             rel="noreferrer"
             className={`${styles.paragraph} ${styles.flexBetween} gap-2 text-[20px] text-dimRose md:flex hidden`}
           >
-            <MdMail />
-            <span className={styles.linkHover}>Email</span>
+            <FaAngellist />
+            <span className={styles.linkHover}>Wellfound</span>
           </a>
           <a
             href="https://bit.ly/35RxdGT"
@@ -69,25 +73,49 @@ const Footer = () => (
       <div className="w-60 md:mr-4">
         <h3 className={`${styles.heading2} font-light mb-2.5`}><small>Contact</small></h3>
         <ul className="text-[23px] font-thin text-dimWhite md:flex flex-col space-y-4">
-          <li><a href="mailto:ricardo_valtierra@outlook.com" className={styles.linkHover}>Email me</a></li>
-          <li><a href="https://calendly.com/ricardo_valtierra/work-dev-chat" className={styles.linkHover}>Chat with me</a></li>
-          <li><a href="https://api.whatsapp.com/send?phone=524776777394&text=Let's talk about software engineering" className={styles.linkHover}>Message me</a></li>
+          <li>
+            <a href="mailto:ricardo_valtierra@outlook.com" className={styles.linkHover}>Email me</a>
+          </li>
+          <li>
+            <a
+              href="https://calendly.com/ricardo_valtierra/work-dev-chat"
+              target="_blank"
+              rel="noreferrer"
+              className={styles.linkHover}
+            >
+              Chat with me
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://api.whatsapp.com/send?phone=524776777394&text=Let's talk about software engineering"
+              target="_blank"
+              rel="noreferrer"
+              className={styles.linkHover}
+            >
+              Message me
+            </a>
+          </li>
         </ul>
       </div>
       <div className="w-60 md:ml-4">
         <h3 className={`${styles.heading2} font-light mb-2.5 md:mt-0 mt-[60px]`}><small>General</small></h3>
         <ul className="text-[23px] font-thin text-dimWhite md:flex flex-col space-y-4">
-          <li><a href="https://medium.com/@ricardo_valtierra" className={styles.linkHover}>Medium</a></li>
           <li>
             <Link to="/mission-&-vision" className={`${styles.linkHover}`}>
               Mission & Vision
             </Link>
           </li>
+          <li>
+            <a href="https://medium.com/@ricardo_valtierra" target="_blank" rel="noreferrer" className={styles.linkHover}>Medium</a>
+          </li>
         </ul>
       </div>
     </div>
     <div className={`${styles.flexBetween} col-span-2 md:ml-[13px] md:mt-0 mt-12`}>
-      <p className="w-full md:text-[17px] text-[1.1rem] md:text-start text-center text-dimWhite md:font-light font-thin md:tracking-widest">
+      <p
+        className="w-full md:text-[17px] text-[1.1rem] md:text-start text-center text-dimWhite md:font-light font-thin md:tracking-widest"
+      >
         All rights reserved &copy; Ricardo Valtierra 2022.
       </p>
     </div>
