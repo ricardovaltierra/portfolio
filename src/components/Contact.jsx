86a9142f4e786961f4084fe373b5a4c5ca0b0e96
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-11-23 09:30:09 -0600

    Create Contact component and add it to Navbar

---
+++ src/components/Contact.jsx
@@ -0,0 +1,11 @@
+import styles from '../style';
+
+const Contact = () => (
+  <section className={`${styles.section}`}>
+    <p className="">
+      🚧️ In construction 🚧️
+    </p>
+  </section>
+);
+
+export default Contact;
