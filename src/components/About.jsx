b96e0ed3dabed214dfa10dcc7ccfcba4593c222c
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-12-08 14:47:35 -0600

    [WIP] Set initial introductory text for about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import styles from '../style';
+// import styles from '../style';
 
 const About = () => (
-  <section className={`${styles.section} md:mx-[65px] mx-[1.7rem]`}>
-    <p className={`${styles.paragraph}`}>
-      🚧️ Under construction 🚧️
+  <section className="flex md:flex-row flex-col md:mx-[65px] mx-[1.7rem] justify-items-stretch my-[3rem]">
+    <p className="font worksans font-thin text-dimWhite md:text-[24px] text-[25px] my-5 md:leading-[40.8px] leading-[32.8px]">
+      Besides my career development, I enjoy spending my time on a
+      variety of activities and projects, such as reading about topics
+      as diverse as general culture, science, and self-improvement.
+      I like to practice some sports like jogging, basketball and
+      anaerobic exercise
+      <br className="mb-10" />
+      In entertainment, I follow a number of comedy and culture genre
+      movies and television shows, I am an aspiring meditator, and
+      I spend the rest of my free time exploring new web technologies
     </p>
   </section>
 );
