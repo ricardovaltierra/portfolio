99996d866998c777bcdf1c1a7a465bb6df1af45c
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-12-30 02:39:52 -0600

    Split paragraph to replace br tag

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 // import styles from '../style';
 
 const About = () => (
-  <section className="flex md:flex-row flex-col md:mx-[65px] mx-[1.7rem] justify-items-stretch my-[3rem]">
-    <p className="font worksans font-thin text-dimWhite md:text-[24px] text-[25px] my-5 md:leading-[40.8px] leading-[32.8px]">
+  <section className="flex flex-col md:mx-[65px] mx-[1.7rem] md:px-5 justify-items-stretch my-[3rem] font-thin text-dimWhite  md:text-[24px] text-[25px]">
+    <p className="my-5 md:leading-[40.8px] leading-[32.8px]">
       Besides my career development, I enjoy spending my time on a
       variety of activities and projects, such as reading about topics
       as diverse as pop culture or neuronal and general sciences.
       I like to practice activities like meditation, anaerobic exercise,
       and some long walks.
-      <br className="mb-10" />
+    </p>
+    <p className="my-5 md:leading-[40.8px] leading-[32.8px]">
       Getting immersed into a healthy lifestyle and veganism,
       I spend the rest of my free time exploring new web technologies.
     </p>
