c8c228e1ca0f816cd605fe4aacdb30003dfbaa47
ricardovaltierra <ricardo_valtierra@outlook.com>
2023-01-11 19:33:32 -0600

    Third update on about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,15 +4,16 @@ import React from 'react';
 const About = () => (
   <section className="flex flex-col md:mx-[65px] mx-[1.7rem] md:px-5 justify-items-stretch my-[3rem] font-thin text-dimWhite  md:text-[24px] text-[25px]">
     <p className="my-5 md:leading-[40.8px] leading-[32.8px]">
-      Besides my career development, I enjoy spending my time on a
-      variety of activities and projects, such as reading about topics
-      as diverse as pop culture or neuronal and general sciences.
-      I like to practice activities like meditation, anaerobic exercise,
+      Besides my career development, I enjoy using my time on
+      a variety of activities or projects, such as reading
+      topics in pop culture, or neuronal and general sciences.
+      Other times you can find me laughing at funny videos or
+      staying up to date with Trevor Noah in the news. I like
+      to practice activities like meditation, anaerobic exercise,
       and some long walks.
     </p>
     <p className="my-5 md:leading-[40.8px] leading-[32.8px]">
-      Getting immersed into a healthy lifestyle and veganism,
-      I spend the rest of my free time exploring new web technologies.
+      When not on all the above, with family or friends, I&apos;m curiously diving into web tech.
     </p>
   </section>
 );
