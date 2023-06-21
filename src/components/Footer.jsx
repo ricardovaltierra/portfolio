7ad77476bba579613eaa483f7e74000c15669a64
ricardovaltierra <ricardo_valtierra@outlook.com>
2023-06-21 18:22:21 -0400

    Update WhatsApp initial message contact

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -88,7 +88,7 @@ const Footer = () => (
           </li>
           <li>
             <a
-              href="https://api.whatsapp.com/send?phone=16474027030&text=Let's talk about software engineering"
+              href="https://api.whatsapp.com/send?phone=16474027030&text=Hey! Let's talk about software engineering!"
               target="_blank"
               rel="noreferrer"
               className={styles.linkHover}
