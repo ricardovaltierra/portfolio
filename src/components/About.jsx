13441a9560f26a4ce8f66678993f0c0854d10ab2
Ricardo Valtierra <ricardo_valtierra@outlook.com>
2024-11-25 17:36:10 -0500

    Update About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const About = () => (
   <section className="flex flex-col md:mx-[65px] mx-[1.7rem] md:px-5 justify-items-stretch my-[3rem] font-thin text-dimWhite  md:text-[24px] text-[25px]">
     <p className="my-5 md:leading-[40.8px] leading-[32.8px]">
-      Besides my career development, I enjoy using my time on
+      In addition to my carreer, I enjoy using my time on
       a variety of activities or projects, such as reading
       topics in pop culture, or neuronal and general sciences.
       Other times you can find me knowing the city better. I like
