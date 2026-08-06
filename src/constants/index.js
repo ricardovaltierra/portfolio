7a350025e1fae78462866022ff651f78499296c8
Ricardo Valtierra <ricardo_valtierra@outlook.com>
2026-08-06 10:05:44 -0500

    Update cloudinary image URL attempt #3

--- src/constants/index.js
+++ src/constants/index.js
@@ -67,5 +67,5 @@ export const siteMetadata = {
   siteName: 'ricardovaltierra.online',
   locale: 'en_US',
   author: 'Ricardo Valtierra (who else?)',
-  image: 'https://res.cloudinary.com/as0yphqi/image/upload/v1786028348/ricardovaltierra.online__Nest_Hub_pl7yun.png',
+  image: 'https://res.cloudinary.com/as0yphqi/image/upload/v1786028701/ricardovaltierra.online__Nest_Hub_pqjszp.png',
 };
